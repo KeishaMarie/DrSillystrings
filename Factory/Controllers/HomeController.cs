c6d9c960e942177e2c87942b1438f04ba658b166
Keisha Hepner <kmhepner90@gmail.com>
2022-10-21 21:47:29 -0700

    Update all controllers

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
+      return View();
+    }
+  }
+}
